cf11972e08658f15a36a1ad5775241aea6b6ca5b
ahmedb <ahmed.bayoumy@mail.mcgill.ca>
2022-05-10 02:54:20 -0400

    Version 1.0.0

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    long_description = readme_file.read()
+
+
+setup(
+    name="BM_DFO",
+    version="1.0.0",
+    description="Python package that benchmark DFO algorithms mainly OMADS.",
+    long_description= long_description,
+    long_description_content_type="text/markdown",
+    author="Ahmed H. Bayoumy",
+    license="GNU LESSER GENERAL PUBLIC LICENSE",
+    classifiers=[
+        "Development Status :: 1 - release"
+        "Intended Audience :: Developers"
+        "License :: OSI Approved :: GNU LGPL"
+    ],
+    keywords="Derivative free optimization",
+    packages=find_packages(),
+    install_requires="requests>=2",
+    python_requires="~=3.8"
+)
